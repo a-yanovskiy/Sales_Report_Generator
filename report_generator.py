47d5d71b48d16a118e7b5d140ce81a0c1fe6b783
Artem <a.e.yanovskiy@gmail.com>
2022-06-27 11:32:34 +0300

    refactor: move generator to another module

---
+++ report_generator.py
@@ -0,0 +1,56 @@
+import re
+import sales_report_generator.format_report as format
+
+DISH_SMILE = '\U0001F34B'
+LEADERS_SMILE = '\U00002734'
+GROWS_SMILE = '\U00002714'
+FALLS_SMILE = '\U0000274C'
+DELETE_WORD_IN_DISH = ' Свежов'
+
+
+def generate_report(dataframe, result='', dish=''):
+    df_first_column = dataframe.iloc[:, 0:1]
+    df_second_column = dataframe.iloc[:, 1:2]
+    
+    if df_first_column.empty:
+        return result
+    else:
+        dish = re.split('[0-9]', df_first_column.columns[0])[0].strip()
+        
+        result += '\n' + DISH_SMILE + ' *_' + dish + '_*'
+
+        if not df_second_column.empty:
+            if re.match(r'^Unnamed', df_second_column.columns[0]):
+                df_rest_columns = dataframe.iloc[:, 2:]
+                comparing = format.df_to_dict(df_second_column)
+
+                grows_sales = format.sort_and_filter_dict_by_items(
+                    comparing, 'positive')
+                result += '\n' + GROWS_SMILE + ' _Прирост:_ ' + format.format_dict(grows_sales)
+
+                falls_sales = format.sort_and_filter_dict_by_items(
+                    comparing, 'negative')
+                result += '\n' + FALLS_SMILE + ' _Падение:_ ' + format.format_dict(falls_sales)
+
+            else:
+                df_rest_columns = dataframe.iloc[:, 1:]
+        else:
+            df_rest_columns = dataframe.iloc[:, 1:]
+        
+        f = format.df_to_dict(df_first_column)
+        sorted_ = format.sort_and_filter_dict_by_items(f)
+        sales_leaders = format.format_dict(sorted_, DELETE_WORD_IN_DISH)
+        
+        result += '\n' + LEADERS_SMILE + ' _ЛИДЕРЫ ПРОДАЖ:_ ' + sales_leaders + '\n'
+
+        return generate_report(df_rest_columns, result, dish)
+
+
+path1 = 'd:\Downloads\Акция неделька (2).xlsx'
+path2 = 'd:\Downloads\Отчет Акция за прошлую неделю.xlsx'
+
+# file1 = read_xlsx(path1)
+# print(generate_report(file1))
+
+file2 = format.read_xlsx(path2)
+print(generate_report(file2))
--- sales_report_generator/format_report.py
+++ sales_report_generator/format_report.py
@@ -40,10 +40,10 @@ def sort_and_filter_dict_by_items(dictionary, filter='positive') -> dict:
     return series.to_dict()
 
 
-def format_dict(dict) -> str:
+def format_dict(dict, word_to_be_deleted) -> str:
     result = ''
     for market, value in dict.items() :
-        market = re.split(' ИП', market)[0]
+        market = re.split(word_to_be_deleted, market, maxsplit=1)[0]
         if value == value:
                 meter = 'кг'
                 if re.match(r'^-?\d+(?:\.\d+)$', str(value)) is None:
@@ -68,45 +68,3 @@ def df_to_dict(df_series, result={}) -> dict:
         value = item.values[0][0]
         result[index] = value
         return df_to_dict(rest_items, result)
-
-
-def generate_report(dataframe, result='', dish=''):
-    df_first_column = dataframe.iloc[:, 0:1]
-    df_second_column = dataframe.iloc[:, 1:2]
-    
-    if df_first_column.empty:
-        return result
-    else:
-        dish = re.split('[0-9]', df_first_column.columns[0])[0].strip()
-        
-        result += '\n \U0001F34B *_' + dish + '_*'
-
-        if not df_second_column.empty:
-            if re.match(r'^Unnamed', df_second_column.columns[0]):
-                df_rest_columns = dataframe.iloc[:, 2:]
-                comparing = df_to_dict(df_second_column)
-
-                grows_sales = sort_and_filter_dict_by_items(comparing, 'positive')
-                result += '\n \U00002714 _Прирост:_ ' + format_dict(grows_sales)
-
-                falls_sales = sort_and_filter_dict_by_items(comparing, 'negative')
-                result += '\n \U0000274C _Падение:_ ' + format_dict(falls_sales)
-
-            else:
-                df_rest_columns = dataframe.iloc[:, 1:]
-        else:
-            df_rest_columns = dataframe.iloc[:, 1:]
-        
-        f = df_to_dict(df_first_column)
-        sorted_ = sort_and_filter_dict_by_items(f)
-        sales_leaders = format_dict(sorted_)
-        
-        result += '\n \U00002734 _ЛИДЕРЫ ПРОДАЖ:_ ' + sales_leaders + '\n'
-
-        return generate_report(df_rest_columns, result, dish)
-
-
-# path1 = 'd:\Downloads\акции1.xlsx'
-# path2 = 'd:\Downloads\новинки 08.04.22.xlsx'
-# file = read_xlsx(path1)
-# print(generate_report(file))
