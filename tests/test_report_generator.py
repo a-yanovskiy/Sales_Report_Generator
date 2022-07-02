97d494d1581ec9cb2b7f8fb74537c7fc62cbe98b
Artem <a.e.yanovskiy@gmail.com>
2022-07-02 19:07:44 +0300

    test: add tests

--- tests/test_report_generator.py
+++ tests/test_report_generator.py
@@ -1,14 +1,34 @@
 import sales_report_generator.report_generator as report
+import pytest
+from format_report import read_xlsx
 
 REPORT1_PATH = "tests/fixtures/stocks_1.xlsx"
 RESULT1_PATH = "tests/fixtures/result_1.txt"
 
+REPORT2_PATH = "tests/fixtures/stocks_2.xlsx"
+RESULT2_PATH = "tests/fixtures/result_2.txt"
 
-def test_generate_report():
-    file = report.read_xlsx(REPORT1_PATH)
-    report_from_file = report.generate_report(file)
+DISH_ADD = 'Dish:'
+LEADERS_ADD = 'Sales Leaders:'
+GROWS_ADD = 'Growth:'
+FALLS_ADD = 'Falls:'
+DISH_DEL = 'Yammy'
 
-    with open(RESULT1_PATH, 'r', encoding='utf-8') as f:
+
+@pytest.mark.parametrize("report_path, result_path", [
+                        (REPORT1_PATH, RESULT1_PATH),
+                        (REPORT2_PATH, RESULT2_PATH)])
+def test_generate_report(report_path, result_path):
+    file = read_xlsx(report_path)
+
+    generated_report = report.generate_report(file,
+                                              dish_add_text=DISH_ADD,
+                                              leaders_add_text=LEADERS_ADD,
+                                              grows_add_text=GROWS_ADD,
+                                              falls_add_text=FALLS_ADD,
+                                              dish_del_text=DISH_DEL)
+
+    with open(result_path, 'r', encoding='utf-8') as f:
         result = f.read()
 
-    assert report_from_file == result
+    assert generated_report == result
